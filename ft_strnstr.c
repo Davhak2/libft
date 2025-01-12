f8c51d2ec98e8d5ccdbe22b138e264cd5dba2bc9
Davit Hakobyan <davihako@c2r11s6.42yerevan.am>
2025-01-12 17:02:12 +0400

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,25 @@
 /*   By: davihako <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 14:42:34 by davihako          #+#    #+#             */
-/*   Updated: 2025/01/11 14:42:34 by davihako         ###   ########.fr       */
+/*   Updated: 2025/01/12 17:01:11 by davihako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *str, const char *key, size_t len)
 {
+	size_t	keylen;
 
+	if (!(*key) || !key)
+		return ((char *)str);
+	keylen = ft_strlen(key);
+	while (*str && len >= keylen)
+	{
+		if (*str == *key && !(ft_strncmp(str, key, keylen)))
+			return ((char *)str);
+		++str;
+		--len;
+	}
+	return (NULL);
 }
